4b926703a52d7706740d4316af6683d04f7e89bc
mayor-creator <67986615+mayor-creator@users.noreply.github.com>
2024-02-25 17:58:02 -0500

    add resetCalculation

--- app/index.js
+++ app/index.js
@@ -1,10 +1,8 @@
-let bill = document.getElementById("bill").value;
-let people = document.getElementById("people").value;
-let selectTip = document.querySelectorAll('input[name="tip"]');
-let costButton = document.querySelector(".cost__button");
-let resetButton = document.querySelector(".reset__button");
-
 const tipCalculator = () => {
+	let bill = document.getElementById("bill").value;
+	let people = document.getElementById("people").value;
+	let selectTip = document.querySelectorAll('input[name="tip"]');
+
 	//converting bill, people and tip values to floating point
 	let amountOfBill = parseFloat(bill);
 	let numberOfPeople = parseFloat(people);
@@ -34,17 +32,24 @@ const tipCalculator = () => {
 };
 
 const resetCalculation = () => {
-	// let resetBill = document.getElementById("bill").value;
-	// resetBill = `${placeholder}`;
-	// let resetPeople = document.getElementById("people").value;
-	// resetPeople = `${placeholder}`;
+	//reset bill
+	let resetBill = document.getElementById("bill");
+	resetBill.value = "0";
+	resetBill.style.color = "#5e7a7d";
+	//reset number of people
+	let resetPeople = document.getElementById("people");
+	resetPeople.value = "0";
+	resetPeople.style.color = "#5e7a7d";
 
-	let resetTipAmount = document.querySelector(".tip__amount");
+	//reset tip amount and total
 	let innerValue = "$0.00";
+	let resetTipAmount = document.querySelector(".tip__amount");
 	resetTipAmount.innerHTML = `${innerValue}`;
 	let resetTotalPeople = document.querySelector(".total__person");
 	resetTotalPeople.innerHTML = `${innerValue}`;
 };
 
+let costButton = document.querySelector(".cost__button");
+let resetButton = document.querySelector(".reset__button");
 costButton.addEventListener("click", tipCalculator);
 resetButton.addEventListener("click", resetCalculation);
